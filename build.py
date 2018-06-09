eaf512d4d4f14f9f2912839cfc1b1adba982a359
Marco Bonelli <mb5.marcob@gmail.com>
2018-06-09 23:24:03 +0200

    Build: get ready to autodeploy.

--- build.py
+++ build.py
@@ -16,17 +16,14 @@ def say(s, *args):
 	sys.stderr.write(s.format(*args))
 
 def say_pad(s, pad, purge=False):
-	if purge:
-		s = re.sub(r'\s?Validating .*|Validation results: [^\s]+\s?', '', s)
-
 	for l in s.split('\n'):
 		if l.strip():
 			sys.stderr.write(pad + ' ' + l + '\n')
 
 def get_args():
 	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('-r', '--release', action='store_true', help='create GitHub release')
-	parser.add_argument('-d', '--deploy', action='store_true', help='deploy built target')
+	parser.add_argument('--release', action='store_true', help='create GitHub release')
+	parser.add_argument('--deploy', action='store_true', help='deploy extensions')
 	parser.add_argument('--build-dir', default='./build', metavar='PATH', help='build directory')
 	parser.add_argument('target', nargs='?', default='all', help='target to build, one of: ' + ', '.join(sorted(TARGETS)))
 
@@ -55,41 +52,27 @@ def copy_source(dest_dir):
 		else:
 			shutil.copy2(fname, dest_dir)
 
-def create_browser_dir(build_dir, browser_name):
+def get_browser_dirs(build_dir, browser_name):
 	browser_dir     = os.path.join(build_dir, browser_name)
 	browser_src_dir = os.path.join(browser_dir, 'src')
 
-	if os.path.isdir(browser_dir):
-		shutil.rmtree(browser_dir)
+	return browser_dir, browser_src_dir
+
+def create_browser_dirs(build_dir, browser_name):
+	bdir, sdir = get_browser_dirs(build_dir, browser_name)
+
+	if os.path.isdir(bdir):
+		shutil.rmtree(bdir)
 
 	say('[Build/{}] Copying sources...\r', browser_name)
-	copy_source(browser_src_dir)
+	copy_source(sdir)
 	say('[Build/{}] Copying sources... done.\n', browser_name)
 
 	say('[Build/{}] Merging manifest...\r', browser_name)
-	merge_json('manifest.json', 'manifest.' + browser_name + '.json', os.path.join(browser_src_dir, 'manifest.json'))
+	merge_json('manifest.json', 'manifest.' + browser_name + '.json', os.path.join(sdir, 'manifest.json'))
 	say('[Build/{}] Merging manifest... done.\n', browser_name)
 
-	return browser_dir, browser_src_dir
-
-def clean_browser_dir(bdir, sdir, browser_name):
-	say('[Build/{}] Cleaning build dir...\r', browser_name)
-	shutil.rmtree(sdir)
-	say('[Build/{}] Cleaning build dir... done.\n', browser_name)
-
-	say('[Build/{}] Gathering artifacts...\r', browser_name)
-
-	artifacts = []
-	for fname in os.listdir(bdir):
-		dot      = fname.rfind('.')
-		newfname = os.path.join(bdir, fname[:dot] + '_' + browser_name + fname[dot:])
-
-		os.rename(os.path.join(bdir, fname), newfname)
-		artifacts.append((newfname, mimetypes.guess_type(newfname)[0]))
-
-	say('[Build/{}] Gathering artifacts... done.\n', browser_name)
-
-	return artifacts
+	return bdir, sdir
 
 def web_ext_build(bdir, sdir, browser_name):
 	say('[Build/{}] Running web-ext build...\r', browser_name)
@@ -108,13 +91,14 @@ def web_ext_build(bdir, sdir, browser_name):
 		say('[Build/{}] Error: web-ext exited with code {}, aborting.\n', browser_name, sp.returncode)
 		exit(1)
 
-def web_ext_sign(bdir, sdir, browser_name):
-	ENV_AMO_JWT_ISSUER = os.getenv('AMO_JWT_ISSUER')
-	ENV_AMO_JWT_SECRET = os.getenv('AMO_JWT_SECRET')
+	say('[Build/{}] Done.\n', browser_name)
 
+def web_ext_sign(bdir, sdir, browser_name):
 	if not (ENV_AMO_JWT_ISSUER and ENV_AMO_JWT_SECRET):
-		say('[Build/{}] Error: missing one or more needed environment variables, aborting.\n', browser_name)
+		say('[Deploy/{}] Error: missing one or more needed environment variables, aborting.\n', browser_name)
 		exit(1)
+
+	say('[Deploy/{}] Signing extension with web-ext sign...\r', browser_name)
 
 	sp = subprocess.Popen(
 		[
@@ -129,40 +113,25 @@ def web_ext_sign(bdir, sdir, browser_name):
 	)
 
 	out, _ = sp.communicate()
-	say_pad(out, '[Build/{}]'.format(browser_name), True)
+
+	say('[Deploy/{}] Signing extension with web-ext sign... done.\n', browser_name)
 
 	if sp.returncode != 0:
-		say('[Build/{}] Error: web-ext exited with code {}, aborting.\n', browser_name, sp.returncode)
+		say('[Deploy/{}] Error: web-ext sign exited with code {}, aborting.\n\n', browser_name, sp.returncode)
+		say(out)
 		exit(1)
 
-def build_chrome(build_dir, deploy=False):
-	bdir, sdir = create_browser_dir(build_dir, 'chrome')
+	say('[Deploy/{}] Done.\n', browser_name)
 
-	if deploy:
-		# TODO
-		pass
-	else:
-		web_ext_build(bdir, sdir, 'chrome')
+def build_chrome(build_dir):
+	bdir, sdir = create_browser_dirs(build_dir, 'chrome')
+	web_ext_build(bdir, sdir, 'chrome')
 
-	artifacts = clean_browser_dir(bdir, sdir, 'chrome')
+def build_firefox(build_dir):
+	bdir, sdir = create_browser_dirs(build_dir, 'firefox')
+	web_ext_build(bdir, sdir, 'firefox')
 
-	return artifacts
-
-def build_firefox(build_dir, deploy=False):
-	bdir, sdir = create_browser_dir(build_dir, 'firefox')
-
-	say('[Build/firefox] Running web-ext sign...\r')
-
-	if deploy:
-		web_ext_sign(bdir, sdir, 'firefox')
-	else:
-		web_ext_build(bdir, sdir, 'firefox')
-
-	artifacts = clean_browser_dir(bdir, sdir, 'firefox')
-
-	return artifacts
-
-def build(repo, target, build_dir, deploy):
+def build(repo, target, build_dir):
 	if os.getcwd() == os.path.abspath(build_dir):
 		say('[Build] Error: cannot build in source directory.\n')
 		exit(1)
@@ -174,24 +143,76 @@ def build(repo, target, build_dir, deploy):
 			say('[Build] Error: unable to create build directory "{}", aborting.\n', build_dir)
 			exit(1)
 
-	built    = []
 	tag_name = repo.git.describe('--tags')
-	builders = TARGETS.get(target)
 
-	if builders is None:
+	if target not in TARGETS.keys():
 		say('[Build] Error: unknown target "{}", aborting.\n', target)
 		exit(1)
 
 	say('[Build] Target: {}.\n', target)
 	say('[Build] Building {} ({}).\n', tag_name, repo.head.commit.hexsha)
 
-	for builder in builders:
-		assets = builder(build_dir, deploy)
-		built.extend(assets)
+	for builder in TARGETS[target]['build']:
+		builder(build_dir)
 
 	say('[Build] Done.\n')
 
-	return built
+def deploy_chrome(_):
+	say("[Deploy/chrome] Unfortunately, Google doesn't provide automatic\n")
+	say('[Deploy/chrome] deployment options for the Chrome Web Store :(\n')
+	say('[Deploy/chrome] Done.\n')
+
+def deploy_firefox(build_dir):
+	bdir, sdir = get_browser_dirs(build_dir, 'firefox')
+	web_ext_sign(bdir, sdir, 'firefox')
+
+def deploy(target, build_dir):
+	if target not in TARGETS.keys():
+		say('[Deploy] Error: unknown target "{}", aborting.\n', target)
+		exit(1)
+
+	say('[Deploy] Target: {}.\n', target)
+
+	for deployer in TARGETS[target]['deploy']:
+		deployer(build_dir)
+
+def clean_build_dir(build_dir):
+	for target in filter(lambda k: k != 'all', TARGETS.keys()):
+		_, sdir = get_browser_dirs(build_dir, target)
+
+		if os.path.isdir(sdir):
+			shutil.rmtree(sdir)
+
+def rename_assets(build_dir):
+	for target in filter(lambda k: k != 'all', TARGETS.keys()):
+		bdir, _ = get_browser_dirs(build_dir, target)
+
+		if os.path.isdir(bdir):
+			for fname in os.listdir(bdir):
+				ext        = fname[fname.rfind('.'):]
+				clean_name = re.sub(r'([\w_]+-\d+\.\d+\.\d+).*', r'\1', fname)
+				newfname   = clean_name + '-' + target + ext
+
+				os.rename(os.path.join(bdir, fname),  os.path.join(bdir, newfname))
+
+def get_assets(build_dir, deployed):
+	say('[Release] Gathering assets...\r')
+
+	assets = []
+
+	for target in filter(lambda k: k != 'all', TARGETS.keys()):
+		bdir, _ = get_browser_dirs(build_dir, target)
+
+		for fname in os.listdir(bdir):
+			if deployed and fname[-4:] == '.zip':
+				continue
+
+			fullname = os.path.join(bdir, fname)
+			assets.append((fullname, mimetypes.guess_type(fullname)[0]))
+
+	say('[Release] Gathering assets... done.\n')
+
+	return assets
 
 def get_head_tag_name(repo):
 	tag_name = repo.git.describe('--tags')
@@ -200,6 +221,23 @@ def get_head_tag_name(repo):
 		if tag.name == tag_name:
 			return tag_name
 	return None
+
+def check_releasable(tag_name):
+	if not (ENV_GH_RELEASE_BRANCH and ENV_TRAVIS_BRANCH and ENV_TRAVIS_PR):
+		say('[Release] Error: missing one or more needed environment variables, aborting.\n')
+		exit(1)
+
+	if ENV_TRAVIS_PR == '1':
+		say('[Release] Skipping release: this is a pull request.')
+		exit(0)
+
+	if ENV_TRAVIS_BRANCH != ENV_GH_RELEASE_BRANCH:
+		say('[Release] Skipping release: current branch ({}) is not designed release branch ({}).\n', ENV_TRAVIS_BRANCH, ENV_GH_RELEASE_BRANCH)
+		exit(0)
+
+	if tag_name is None:
+		say('[Release] Skipping release: HEAD not pointing to a tag.\n')
+		exit(0)
 
 def get_changelog(fname):
 	changelog = []
@@ -222,37 +260,18 @@ def get_changelog(fname):
 
 	return head + body
 
-def release(repo, assets=[]):
+def release_create(repo):
 	import github3
 
-	ENV_TOKEN            = os.getenv('GH_OAUTH_TOKEN')
-	ENV_RELEASE_BRANCH   = os.getenv('GH_RELEASE_BRANCH')
-	ENV_RELEASE_BASENAME = os.getenv('GH_RELEASE_BASENAME')
-
-	ENV_TRAVIS_REPO_SLUG = os.getenv('TRAVIS_REPO_SLUG')
-	ENV_TRAVIS_BRANCH    = os.getenv('TRAVIS_BRANCH')
-	ENV_TRAVIS_PR        = os.getenv('TRAVIS_PULL_REQUEST')
-
-	if ENV_TRAVIS_PR == '1':
-		say('[Release] Skipping release: pull requests.')
-		exit(0)
-
-	if not (ENV_TOKEN and ENV_RELEASE_BRANCH and ENV_RELEASE_BASENAME and ENV_TRAVIS_REPO_SLUG and ENV_TRAVIS_BRANCH):
+	if not (ENV_GH_TOKEN and ENV_GH_RELEASE_BASENAME and ENV_TRAVIS_REPO_SLUG):
 		say('[Release] Error: missing one or more needed environment variables, aborting.\n')
 		exit(1)
 
-	tag_name   = get_head_tag_name(repo)
-
-	if ENV_TRAVIS_BRANCH != ENV_RELEASE_BRANCH:
-		say('[Release] Skipping release: current branch ({}) is not designed release branch ({}).\n', ENV_TRAVIS_BRANCH, ENV_RELEASE_BRANCH)
-		exit(0)
-
-	if tag_name is None:
-		say('[Release] Skipping release: HEAD not pointing to a tag.\n')
-		exit(0)
+	tag_name = get_head_tag_name(repo)
+	check_releasable(tag_name)
 
 	user, repo = ENV_TRAVIS_REPO_SLUG.split('/')
-	release_name = ENV_RELEASE_BASENAME + ' ' + tag_name
+	release_name = ENV_GH_RELEASE_BASENAME + ' ' + tag_name
 	release_is_pre = any(s in tag_name for s in ('alpha', 'beta', 'pre'))
 
 	say('[Release] Releasing {}.\n', release_name)
@@ -261,7 +280,7 @@ def release(repo, assets=[]):
 	release_body = get_changelog('CHANGELOG.md')
 	say('[Release] Parsing changelog... done.\n')
 
-	gh_repo    = github3.login(token=ENV_TOKEN).repository(user, repo)
+	gh_repo    = github3.login(token=ENV_GH_TOKEN).repository(user, repo)
 	gh_release = None
 
 	say('[Release] Creating release...\r')
@@ -273,16 +292,15 @@ def release(repo, assets=[]):
 		gh_release = gh_repo.create_release(tag_name, name=release_name, body=release_body, prerelease=release_is_pre)
 		say('[Release] Creating release... done.\n')
 
+	return gh_release
+
+def release_upload_assets(gh_release, assets):
 	for fname, mimetype in assets:
-		say('[Release] Uploading {}...\r', fname)
+		say('[Release/upload] Uploading {}...\r', fname)
 		gh_release.upload_asset(mimetype, os.path.split(fname)[-1], open(fname, 'rb').read())
-		say('[Release] Uploading {}... done.\n', fname)
+		say('[Release/upload] Uploading {}... done.\n', fname)
 
 	say('[Release] Done.\n')
-
-def deploy():
-	# TODO
-	pass
 
 ###############################################################
 
@@ -294,19 +312,47 @@ FILES = [
 ]
 
 TARGETS = {
-	'chrome': [build_chrome],
-	'firefox': [build_firefox],
-	'all': [build_chrome, build_firefox]
+	'chrome': {
+		'build': [build_chrome],
+		'deploy': [deploy_chrome]
+	},
+	'firefox': {
+		'build': [build_firefox],
+		'deploy': [deploy_firefox]
+	},
+	'all': {
+		'build': [build_chrome, build_firefox],
+		'deploy': [deploy_chrome, deploy_firefox]
+	},
 }
 
 if __name__ == '__main__':
-	args     = get_args()
-	git_repo = git.Repo()
+	ENV_GH_TOKEN            = os.getenv('GH_OAUTH_TOKEN')
+	ENV_GH_RELEASE_BASENAME = os.getenv('GH_RELEASE_BASENAME')
+	ENV_GH_RELEASE_BRANCH   = os.getenv('GH_RELEASE_BRANCH')
 
-	assets = build(git_repo, args.target, args.build_dir, args.deploy)
+	ENV_AMO_JWT_ISSUER      = os.getenv('AMO_JWT_ISSUER')
+	ENV_AMO_JWT_SECRET      = os.getenv('AMO_JWT_SECRET')
+
+	ENV_TRAVIS_BRANCH       = os.getenv('TRAVIS_BRANCH')
+	ENV_TRAVIS_PR           = os.getenv('TRAVIS_PULL_REQUEST')
+	ENV_TRAVIS_REPO_SLUG    = os.getenv('TRAVIS_REPO_SLUG')
+
+	args       = get_args()
+	git_repo   = git.Repo()
+	gh_release = None
+
+	build(git_repo, args.target, args.build_dir)
 
 	if args.release:
-		release(git_repo, assets)
+		gh_release = release_create(git_repo)
 
 	if args.deploy:
-		deploy()
+		deploy(args.target, args.build_dir)
+
+	clean_build_dir(args.build_dir)
+	rename_assets(args.build_dir)
+
+	if args.release:
+		assets = get_assets(args.build_dir, args.deploy)
+		release_upload_assets(gh_release, assets)
